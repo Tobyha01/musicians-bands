0ef4f2fd565ea79158169f8ca48cd6966e03644c
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-17 13:09:23 +0000

    Added a showCount property to Band.js

--- Band.js
+++ Band.js
@@ -1,9 +1,10 @@
 const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Band model
-let Band = sequelize.define("band", {
+let Band = sequelize.define("Band", {
     name: Sequelize.STRING,
-    genre: Sequelize.STRING
+    genre: Sequelize.STRING,
+    showCount: Sequelize.NUMBER
 })
 
 module.exports = {
