ed75b53c854fe4acba79980773c2cdc7191a2bf7
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-19 15:35:10 +0000

    Added a test to check many songs could be added to many bands, removed line comments for other tests once many to many relationaship had been checked

--- index.test.js
+++ index.test.js
@@ -34,10 +34,10 @@ describe('Band and Musician Models', () => {
         expect(musician2.instrument).toBe("Piano");
     })
 
-    test("can create a song", async function() {
+    test("can create a Song", async function() {
         const song1 = await Song.create({title: "song1", year: 2023})
         const song2 = await Song.create({title: "song2", year: 2023})
-        console.log("song")
+        // console.log("song")
         expect(song1.title).toBe("song1")
         expect(song2.title).toBe("song2")
         expect(song1.year).toBe(2023)
@@ -45,17 +45,29 @@ describe('Band and Musician Models', () => {
         // expect(song).toHaveProperty({title: "song1", year: 2023})
     })
 
+    test("can add multiple songs to multiple bands", async function() {
+        const band1 = await Band.findByPk(1)
+        const band2 = await Band.findByPk(2)
+        await band1.addSongs([1,2])
+        await band2.addSongs([1,2])
+        const song = await band1.getSongs()
+        expect(song[0].title).toBe("song1")
+        expect(song[1].title).toBe("song2")
+        expect(song[0].year).toBe(2023)
+        expect(song[1].year).toBe(2023)
+    })
+
     test("can add many musicians to one band", async function() {
         const band = await Band.findByPk(1)
         await band.addMusicians([1,2])
-        const musicians = await band.getMusicians([1,2])
+        const musician = await band.getMusicians([1,2])
         // expect(musicians[0]).toEqual({id: 1, name: "Steve", instrument: "Drums", bandId: 1})
-        expect(musicians[0].name).toBe("Steve")
-        expect(musicians[1].name).toBe("Bob")
-        expect(musicians[0].instrument).toBe("Drums")
-        expect(musicians[1].instrument).toBe("Piano")
-        expect(musicians[0].bandId).toBe(1)
-        expect(musicians[1].bandId).toBe(1)
+        expect(musician[0].name).toBe("Steve")
+        expect(musician[1].name).toBe("Bob")
+        expect(musician[0].instrument).toBe("Drums")
+        expect(musician[1].instrument).toBe("Piano")
+        expect(musician[0].bandId).toBe(1)
+        expect(musician[1].bandId).toBe(1)
     })
 
     test("can update a Band", async function () {
@@ -66,13 +78,6 @@ describe('Band and Musician Models', () => {
         expect(updateBand[0].showCount).toBe(2)
     })
     
-    test("can delete a Band", async function () {
-        await Band.destroy({where: {name: "U2", genre: "Pop", showCount: 2}})
-        const deleteBand = await Band.findAll({where: {name: "U2", genre: "Pop", showCount: 2}})
-        expect(deleteBand).not.toBe({name: "U2", genre: "Pop", showCount: 2})
-    })
-
-    
     test("can update a Musician", async function () {
         await Musician.update({name: "Garry", instrument: "Guitar"}, {where: {name: "Steve", instrument: "Drums"}})
         const updateMusician = await Musician.findAll({where: {name: "Garry", instrument: "Guitar"}})
@@ -80,9 +85,15 @@ describe('Band and Musician Models', () => {
         expect(updateMusician[0].instrument).toBe("Guitar")
     })
     
-    test("can delete a Musician", async function () {
-        await Musician.destroy({where: {name: "Garry", instrument: "Guitar"}})
-        const deleteMusician = await Musician.findAll({where: {name: "Garry", instrument: "Guitar"}})
-        expect(deleteMusician).not.toBe({name: "Garry", instrument: "Guitar"})
-    })
+    // test("can delete a Musician", async function () {
+    //     await Musician.destroy({where: {name: "Garry", instrument: "Guitar"}})
+    //     const deleteMusician = await Musician.findAll({where: {name: "Garry", instrument: "Guitar"}})
+    //     expect(deleteMusician).not.toBe({name: "Garry", instrument: "Guitar"})
+    // })
+
+    // test("can delete a Band", async function () {
+    //     await Band.destroy({where: {name: "U2", genre: "Pop", showCount: 2}})
+    //     const deleteBand = await Band.findAll({where: {name: "U2", genre: "Pop", showCount: 2}})
+    //     expect(deleteBand).not.toBe({name: "U2", genre: "Pop", showCount: 2})
+    // })
 })
