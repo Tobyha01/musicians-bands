3fe72e3cae0d4bd1d8f5e302870ad7a3e628a6f8
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-19 11:36:35 +0000

    Imported and exported the song db

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const {Band} = require('./Band')
 const {Musician} = require('./Musician')
+const {Song} = require('./Song')
 
 Musician.belongsTo(Band)
 Band.hasMany(Musician)
 
 module.exports = {
     Band,
-    Musician
+    Musician,
+    Song
 };
