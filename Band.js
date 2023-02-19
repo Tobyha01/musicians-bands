df6346aa7fd27ff1386e9795d7ccda789903e5ed
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-19 12:29:24 +0000

    Changed the db name from a var to a const

--- Band.js
+++ Band.js
@@ -1,7 +1,7 @@
 const {DataTypes, sequelize} = require('./db');
 
 // TODO - define the Band model
-let Band = sequelize.define("band", {
+const Band = sequelize.define("band", {
     name: DataTypes.STRING,
     genre: DataTypes.STRING,
     showCount: DataTypes.NUMBER
--- Musician.js
+++ Musician.js
@@ -1,7 +1,7 @@
 const {DataTypes, sequelize} = require('./db');
 
 // TODO - define the Musician model
-let Musician = sequelize.define("musician", {
+const Musician = sequelize.define("musician", {
     name: DataTypes.STRING,
     instrument: DataTypes.STRING,
 /*     createdAt: DataTypes.BOOLEAN */
